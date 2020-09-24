032c21a72640f2a522fbc801f2cbdf4021f92a69
ahkam-9408 <it18098174@my.sliit.lk>
2020-09-24 13:27:19 +0530

    Initial Commit

---
+++ src/Common/DBConnection.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Common;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Ahmad Ahkam
+ */
+public class DBConnection {
+    
+    
+    private static Connection connection;
+
+	// private DBConnection() {}
+    public static int userid;
+    public static String usertype;
+
+	public static Connection getDBConnection() throws ClassNotFoundException, SQLException {
+                
+                Class.forName("com.mysql.jdbc.Driver");
+                connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/timetable_management", "root", "");
+                
+                return connection;
+	}
+        
+        public static void iud(String sql) throws Exception {
+
+        if (connection == null) {
+            getDBConnection();
+        }
+        connection.createStatement().executeUpdate(sql);
+    }
+
+    public static ResultSet search(String sql) throws Exception {
+
+        if (connection == null) {
+            getDBConnection();
+        }
+        return connection.createStatement().executeQuery(sql);
+    }
+}
